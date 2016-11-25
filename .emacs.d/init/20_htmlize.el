99842102e2a3e96b278ee177a9549dbaa6dbc5bb
kzkh <mac-daisuki@live.jp>
2016-11-25 10:22:25 +0900

    Add init/20_htmlize.el

---
+++ .emacs.d/init/20_htmlize.el
@@ -0,0 +1,10 @@
+;;; 20_htmlize - htmlize configure
+
+;;; Code:
+(autoload 'htmlize-buffer "htmlize" nil t)
+(autoload 'htmlize-file "htmlize" nil t)
+(autoload 'htmlize-many-files "htmlize" nil t)
+(autoload 'htmlize-many-files-dired "htmlize" nil t)
+(autoload 'htmlize-region "htmlize" nil t)
+
+;; (require 'htmlize)
