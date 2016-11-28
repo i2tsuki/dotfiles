eaf875717956b641818438d2595df9a1443d1efe
kizkoh <mac-daisuki@live.jp>
2016-11-28 20:06:11 +0900

    Add X11ONLY ignore rule

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ EXCLUDES="Makefile|LICENSE|clean.sh|install.sh|udev.rules|.git|.gitignore|^.$"
 
 # public
 X11ONLY=""
-[ "$(uname)" = "Darwin" ] && X11ONLY=".Xresources|.Xmodmap|.xsession|.xinitrc|.xprofile"
+[ "$(uname)" = "Darwin" ] && X11ONLY=".Xmodmap|.xsession|.xinitrc|.xprofile"
 for file in $(find . -maxdepth 1 -type f | egrep -v ${EXCLUDES} | egrep -v ${X11ONLY})
 do
     ln -sfv $(readlink -f $file) ${HOME}/
