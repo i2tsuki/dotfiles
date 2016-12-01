c4a709384e1da8b5c1afa4cadc644e0de29841ae
kzkh <mac-daisuki@live.jp>
2016-12-02 01:59:30 +0900

    Add init/20_autocomplete.el

---
+++ .emacs.d/init/20_autocomplete.el
@@ -0,0 +1,43 @@
+;;; 20_autocomplete.el - Auto complete configure
+
+;;; Code:
+;;; Auto complete config
+(require 'auto-complete)
+(require 'auto-complete-config)
+(ac-config-default)
+(define-key ac-complete-mode-map "\C-n" 'ac-next)
+(define-key ac-complete-mode-map "\C-p" 'ac-previous)
+(global-auto-complete-mode t)
+
+;;; Company mode config
+(global-company-mode +1)
+(custom-set-variables
+ '(company-idle-delay nil))
+;; Color setup
+(set-face-attribute 'company-tooltip nil
+                    :foreground "black" :background "lightgrey")
+(set-face-attribute 'company-tooltip-common nil
+                    :foreground "black" :background "lightgrey")
+(set-face-attribute 'company-tooltip-common-selection nil
+                    :foreground "white" :background "steelblue")
+(set-face-attribute 'company-tooltip-selection nil
+                    :foreground "black" :background "steelblue")
+(set-face-attribute 'company-preview-common nil
+                    :background nil :foreground "lightgrey" :underline t)
+(set-face-attribute 'company-scrollbar-fg nil
+                    :background "orange")
+(set-face-attribute 'company-scrollbar-bg nil
+                    :background "gray40")
+(global-set-key (kbd "C-M-i") 'company-complete)
+;; Choose next or previous supplement candidate
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-search-map (kbd "C-n") 'company-select-next)
+(define-key company-search-map (kbd "C-p") 'company-select-previous)
+;; Filter candidates
+(define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+;; Select candidate
+(define-key company-active-map (kbd "C-i") 'company-complete-selection)
+(define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
+
+(company-quickhelp-mode +1)
