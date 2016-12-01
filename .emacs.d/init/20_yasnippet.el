fbcffb4a27fc5c4951b01e9f907cbe155aff26cd
kzkh <mac-daisuki@live.jp>
2016-12-02 01:48:38 +0900

    Add init/20_yasnippet.el

---
+++ .emacs.d/init/20_yasnippet.el
@@ -0,0 +1,15 @@
+;;; 20_yasnippet.el - yasnippet configure
+
+;;; Code:
+(require 'yasnippet)
+;; Initial snippet dir setup
+(setq yas-snippet-dirs
+      '("~/.emacs.d/yasnippet/" 
+        ))
+(yas-global-mode 1)
+
+(custom-set-variables '(yas-trigger-key "TAB"))
+
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
