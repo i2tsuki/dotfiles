652be13fdcd02f9f2617c2fccb57da898f26e794
kzkh <mac-daisuki@live.jp>
2016-12-01 10:40:27 +0900

    Add init/50_org.el

---
+++ .emacs.d/init/50_org.el
@@ -0,0 +1,26 @@
+;;; 50_org.el - Org mode configure
+
+;;; Code:
+(require 'org)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+
+(setq org-directory "~/org")
+(setq org-default-notes-file "~/org/main.org")
+(setq org-agenda-files (list org-default-notes-file ))
+(setq org-log-done t)
+(setq org-use-fast-todo-selection t)
+(setq org-startup-truncated nil)
+
+
+(define-key global-map "\C-cl" 'org-store-link)
+;; (define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-cb" 'org-iswitchb)
+
+(require 'org-install)
+
+;; Display bullet for org
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; Org for remarkjs
+(require 'org-remark)
