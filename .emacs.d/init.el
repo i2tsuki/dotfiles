956f9fd5012540bed4d7362a10023d70b6971434
kzkh <mac-daisuki@live.jp>
2016-12-02 02:16:02 +0900

    Add calculating launch time

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,7 +14,6 @@
 (setq load-path
       (append '("~/.emacs.d/auto-install")
               load-path))
-
 (setq load-path
       (append '("~/.cabal/share/x86_64-linux-ghc-7.10.3/Agda-2.5.1.1/emacs-mode")
               load-path))
@@ -22,3 +21,15 @@
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/init")
 (init-loader-load "~/.emacs.d/init/user")
+
+;; Calculate launch time
+(defun my-time-lag ()
+  (let* ((now (current-time))
+         (min (- (car now) (car my-time-zero)))
+         (sec (- (car (cdr now)) (car (cdr my-time-zero))))
+         (msec (/ (- (car (cdr (cdr now)))
+		     (car (cdr (cdr my-time-zero))))
+		  1000))
+         (lag (+ (* 60000 min) (* 1000 sec) msec)))
+    (message "'.emacs.el' loading time: %d msec." lag)))
+(defvar my-time-zero (current-time))
