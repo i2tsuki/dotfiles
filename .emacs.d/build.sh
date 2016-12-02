bfee3acff0ddce3c78322a3805e3fdf999b68fe3
kzkh <mac-daisuki@live.jp>
2016-12-02 18:31:08 +0900

    Add .emacs.d/build.sh

---
+++ .emacs.d/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -eu
+
+# Clean elc files
+for file in $(find ./init -name '*.elc' -type f)
+do
+    rm -fv ${file}
+done
+
+ELFILE=""
+for file in $(find ./init -name '*.el' -type f -maxdepth 1)
+do
+    ELFILE+="$(pwd)/${file#./} "
+done
+ELFILE="${ELFILE% }"
+
+emacs --batch -l ~/.emacs.d/init.el -f batch-byte-compile ${ELFILE}
