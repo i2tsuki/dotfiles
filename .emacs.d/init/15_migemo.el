2753ad5eabd4d7eeabc08da584f21fd8890ec111
kzkh <mac-daisuki@live.jp>
2016-12-02 12:34:55 +0900

    Add init/15_migemo.el

---
+++ .emacs.d/init/15_migemo.el
@@ -0,0 +1,28 @@
+;;; 15_migemo.el - Migemo configure
+
+;;; Code:
+(when (executable-find "cmigemo")
+  (setq migemo-command "cmigemo")
+  (setq migemo-options '("-q" "--emacs"))
+  (setq migemo-dictionary "/usr/share/migemo/migemo-dict")
+  (setq migemo-regex-dictionary nil)
+  (setq migemo-pattern-alist-length 1000)
+  (setq migemo-coding-system 'utf-8-unix)
+  (load-library "migemo")
+  )
+
+(defvar migemo-coding-system 'utf-8-unix)
+
+;; Helm migemo configure
+(defvar helm-use-migemo t)
+;; 候補が表示されないときがあるので migemo らないように設定
+;; (defadvice helm-c-apropos
+;;   (around ad-helm-apropos activate)
+;;   (let ((helm-use-migemo nil))
+;;     ad-do-it))
+;; (defadvice helm-M-x
+;;   (around ad-helm-M-x activate)
+;;   (let ((helm-use-migemo nil))
+;;     ad-do-it))
+
+(migemo-init)
