46167cd799dc396cd26926c6a050284fca5969e7
kzkh <mac-daisuki@live.jp>
2016-12-09 15:08:24 +0900

    Fix position to remove old file

--- .emacs.d/install.sh
+++ .emacs.d/install.sh
@@ -3,10 +3,10 @@
 set -u
 
 # rm -rfv ./elpa ./elisp
-rm -fv ./load-path.el
 
 emacs --batch -l ~/.emacs.d/package-install.el
 
+rm -fv ./load-path.el
 for file in $(find ./elisp -mindepth 1 -maxdepth 1 -type d)
 do
     echo "(add-to-list 'load-path (locate-user-emacs-file \"${file#./}\"))" >> ./load-path.el
