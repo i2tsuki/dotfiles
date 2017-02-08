afdda0f0084c12cf1e09d3b66b586ec8ff963804
kizkoh <mac-daisuki@live.jp>
2017-02-08 15:08:00 +0900

    Fix install script

--- install.sh
+++ install.sh
@@ -7,10 +7,27 @@ EXCLUDES="Makefile|LICENSE|clean.sh|install.sh|udev.rules|.git|.gitignore|^.$"
 # public
 X11ONLY=""
 [ "$(uname)" = "Darwin" ] && X11ONLY=".Xmodmap|.xsession|.xinitrc|.xprofile"
-for file in $(find . -maxdepth 1 -type f | egrep -v ${EXCLUDES} | egrep -v ${X11ONLY})
-do
-    ln -sfv $(readlink -f $file) ${HOME}/
-done
+
+if [ "$(uname)" = "Darwin" ] ; then
+    for file in $(find . -maxdepth 1 -type f | egrep -v ${EXCLUDES} | egrep -v ${X11ONLY})
+    do
+        ln -sfv $(readlink -f $file) ${HOME}/
+    done
+    for file in $(find . -maxdepth 1 -type d | egrep -v ${EXCLUDES} | egrep -v ${X11ONLY})
+    do
+        ln -sfv $(readlink -f $file) ${HOME}/
+    done
+else
+    for file in $(find . -maxdepth 1 -type f | egrep -v ${EXCLUDES})
+    do
+        ln -sfv $(readlink -f $file) ${HOME}/
+    done
+    for file in $(find . -maxdepth 1 -type d | egrep -v ${EXCLUDES})
+    do
+        ln -sfv $(readlink -f $file) ${HOME}/
+    done
+fi
+
 # secrets
 for file in $(find ./secrets -maxdepth 1 -type f | egrep -v ${EXCLUDES})
 do
