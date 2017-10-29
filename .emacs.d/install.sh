ae6366b3605ee5fc2c232b4aa9047dd846d3c3f1
kizkoh <mac-daisuki@live.jp>
2017-10-30 06:13:07 +0900

    Remove verbose message removing load-path.el

--- .emacs.d/install.sh
+++ .emacs.d/install.sh
@@ -131,7 +131,7 @@ done
 
 emacs --batch -q -l ~/.emacs.d/package-bundle.el
 
-rm -fv ./load-path.el
+rm -f ./load-path.el
 for file in $(find ./elisp -mindepth 1 -maxdepth 1 -type d)
 do
     echo "(add-to-list 'load-path (locate-user-emacs-file \"${file#./}\"))" >> ./load-path.el
