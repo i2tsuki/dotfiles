e54c09f8be436c06ee201bbce65c11c3758c5da0
kizkoh <mac-daisuki@live.jp>
2018-07-13 23:21:06 +0900

    Add groovy-mode config

--- .emacs.d/init/70_conf.el
+++ .emacs.d/init/70_conf.el
@@ -4,3 +4,7 @@
 ;;; Dockerfile-mode
 (require 'dockerfile-mode)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+;;; Groovy-mode
+(require 'groovy-mode)
+(add-to-list 'auto-mode-alist '("Jenkinsfile\\'" . groovy-mode))
